3f6b0b5ec37a7784523226cca860acffe45e5f66
“dongyuekai” <457545648@qq.com>
2021-04-28 17:00:13 +0800

    formik表单验证

--- 02-fomik/src/App.js
+++ 02-fomik/src/App.js
@@ -5,7 +5,20 @@ function App() {
   const formik = useFormik({
     initialValues: {
       username: 'zhangsan',
-      password: '123456',
+      password: '12345',
+    },
+    validate: values => {
+      const errors = {
+      }
+      if (!values.username) {
+        errors.username = '请输入用户名'
+      } else if (values.username.length > 8) {
+        errors.username = '用户名长度不能大于8'
+      }
+      if (values.password.length < 6) {
+        errors.password = '密码长度不能小于6'
+      }
+      return errors
     },
     onSubmit: (values) => {
       console.log(values)
@@ -13,8 +26,22 @@ function App() {
   })
   return (
     <form onSubmit={formik.handleSubmit}>
-      <input type='text' name='username' value={formik.values.username} onChange={formik.handleChange} />
-      <input type='text' name='password' value={formik.values.password} onChange={formik.handleChange} />
+      <input
+        type='text'
+        name='username'
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      <p style={{ color: 'red' }}>{formik.touched.username && formik.errors.username ? formik.errors.username : null}</p>
+      <input
+        type='text'
+        name='password'
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      <p style={{ color: 'red' }}>{formik.touched.password && formik.errors.password ? formik.errors.password : null}</p>
       <input type='submit' />
     </form>
   );
