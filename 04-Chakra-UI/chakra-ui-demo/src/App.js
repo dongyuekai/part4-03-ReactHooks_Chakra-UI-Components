bede3097354479d00d88fc9758dc9a99794c52a8
“dongyuekai” <457545648@qq.com>
2021-04-29 16:01:17 +0800

    主题对象的颜色

--- 04-Chakra-UI/chakra-ui-demo/src/App.js
+++ 04-Chakra-UI/chakra-ui-demo/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     // <Box w='200px' h='100px' bgColor={colorMode === 'dark' ? 'tomato' : 'skyblue'}>
     <Box w='200px' h='100px' bgColor={bgColor}>
-      <Text>当前颜色模式为{colorMode}</Text>
+      <Text bgColor="gray.600">当前颜色模式为{colorMode}</Text>
       <LightMode>
         <Button onClick={toggleColorMode}>按钮1</Button>
       </LightMode>
