f92066f0b7cd07d433eee685e51b0ec69677cab3
“dongyuekai” <457545648@qq.com>
2021-04-29 11:11:13 +0800

    配置支持emotion

--- 03-CSS-IN-JS/emotion-guide/src/App.js
+++ 03-CSS-IN-JS/emotion-guide/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-/** @jsx jsx */
-/** @jsxRuntime classic */
-import { jsx } from '@emotion/react'
 function App() {
   return (
     <div css={{ width: 200, height: 200, background: 'red' }}>App dsfds</div>
