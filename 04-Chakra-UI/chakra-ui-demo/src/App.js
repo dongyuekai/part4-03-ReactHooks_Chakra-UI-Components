60594094a38af7ce563dc4373b00e37ae8ffcd73
“dongyuekai” <457545648@qq.com>
2021-04-29 15:50:25 +0800

    useColorModeValue钩子函数 根据不同模式设置不同值

--- 04-Chakra-UI/chakra-ui-demo/src/App.js
+++ 04-Chakra-UI/chakra-ui-demo/src/App.js
@@ -1,9 +1,11 @@
-import { Button, Box, useColorMode, Text } from '@chakra-ui/react'
+import { Button, Box, useColorMode, Text, useColorModeValue } from '@chakra-ui/react'
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode()
+  const bgColor = useColorModeValue('tomato', 'skyblue')
   return (
-    <Box w='200px' h='100px' bgColor={colorMode === 'dark' ? 'tomato' : 'skyblue'}>
+    // <Box w='200px' h='100px' bgColor={colorMode === 'dark' ? 'tomato' : 'skyblue'}>
+    <Box w='200px' h='100px' bgColor={bgColor}>
       <Text>当前颜色模式为{colorMode}</Text>
       <Button onClick={toggleColorMode}>按钮</Button>
     </Box>
